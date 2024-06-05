80c2df6b5407f7483f6fc02df0c772a2436209fd
Kelvin <kelxir@outlook.com>
2024-06-05 21:29:13 +0000

    Add function to play single round and five games
    The playRound function compares player choices to computer's choices and
    declares the winner of a round whilst the playGame calls playRound func-
    tion in a for loop to play the game five times, updating both player
    score and computer's score and declaring the winner of the entire game.

--- script.js
+++ script.js
@@ -19,9 +19,69 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "paper":
+                    console.log("You lose! paper beats rock.");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log("You win! rock beats scissors.");
+                    humanScore++;
+                    break;
+                default:
+                    console.log("This round is a tie.");
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("You win, paper beats rock!");
+                    humanScore++;
+                    break;
+                case "scissors":
+                    console.log("You lose! scissors beats paper");
+                    computerScore++;
+                    break;
+                default:
+                    console.log("This round is a tie.");
+                    break;
+            }
+            break;    
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    console.log("You lose! rock beats scissors");
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log("You win, scissors beats paper");
+                    humanScore++;
+                    break;
+                default:
+                    console.log("This round is a tie.");
+                    break;
+            } 
+            break;   
+    }
+}
+
+function playGame() {
+    for (let round = 1; round <= 5; round++) {
+        playRound(getHumanChoice(), getComputerChoice());  
+    }
+    if(computerScore>humanScore) {
+        console.log("You lose the computer has donimated. Skynet initiated.");
+    }else if(computerScore<humanScore) {
+        console.log("You win, John connor.");
+    }else if(computerScore===humanScore) {
+        console.log("Its a tie, the battle for control continues.");
+    }
 }
 
 let computerScore = 0;
 let humanScore = 0;
 
-console.log(getHumanChoice());
+playGame();
