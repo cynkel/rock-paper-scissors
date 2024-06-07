aadeea78a2a70fc24f4c768811f624d67549bfe7
Kelvin <kelxir@outlook.com>
2024-06-07 00:22:55 +0000

    Add User Interface
    Buttons have been added to represent either rock, paper or scissors and
    another button to play the game again after five rounds.

--- script.js
+++ script.js
@@ -23,65 +23,103 @@ function playRound(humanChoice, computerChoice) {
         case "rock":
             switch(computerChoice) {
                 case "paper":
-                    console.log("You lose! paper beats rock.");
+                    statusContainer.textContent = "You lose! paper beats rock.";
                     computerScore++;
                     break;
                 case "scissors":
-                    console.log("You win! rock beats scissors.");
+                    statusContainer.textContent = "You win! rock beats scissors.";
                     humanScore++;
                     break;
                 default:
-                    console.log("This round is a tie.");
+                    statusContainer.textContent = "This round is a tie.";
                     break;
             }
+            displayScore();
             break;
         case "paper":
             switch(computerChoice) {
                 case "rock":
-                    console.log("You win, paper beats rock!");
+                    statusContainer.textContent = "You win, paper beats rock!";
                     humanScore++;
                     break;
                 case "scissors":
-                    console.log("You lose! scissors beats paper");
+                    statusContainer.textContent = "You lose! scissors beats paper";
                     computerScore++;
                     break;
                 default:
-                    console.log("This round is a tie.");
+                    statusContainer.textContent = "This round is a tie.";
                     break;
             }
+            displayScore();
             break;    
         case "scissors":
             switch(computerChoice) {
                 case "rock":
-                    console.log("You lose! rock beats scissors");
+                    statusContainer.textContent = "You lose! rock beats scissors";
                     computerScore++;
                     break;
                 case "paper":
-                    console.log("You win, scissors beats paper");
+                    statusContainer.textContent = "You win, scissors beats paper";
                     humanScore++;
                     break;
                 default:
-                    console.log("This round is a tie.");
+                    statusContainer.textContent = "This round is a tie.";
                     break;
             } 
+            displayScore();
             break;   
     }
 }
 
-function playGame() {
-    for (let round = 1; round <= 5; round++) {
-        playRound(getHumanChoice(), getComputerChoice());  
-    }
-    if(computerScore>humanScore) {
-        console.log("You lose the computer has donimated. Skynet initiated.");
-    }else if(computerScore<humanScore) {
-        console.log("You win, John connor.");
-    }else if(computerScore===humanScore) {
-        console.log("Its a tie, the battle for control continues.");
+function displayScore() {
+    scoreContainer.textContent = `THE MATRIX: ${computerScore}  YOU: ${humanScore}`;
+    if (computerScore === 5) {
+        scoreContainer.textContent = "The computer took over after five battles";
+        scoreContainer.appendChild(playAgain);
+    }else if (humanScore === 5) {
+        scoreContainer.textContent = "You gained freedom after five battles, humanity is free.";
+        scoreContainer.appendChild(playAgain);
     }
 }
+
+function play() {
+    computerScore = 0;
+    humanScore = 0;
+    statusContainer.textContent = "NEW ROUND";
+}
+
 
 let computerScore = 0;
 let humanScore = 0;
 
-playGame();
+
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorsButton = document.createElement("button");
+const statusContainer = document.createElement("div");
+const scoreContainer = document.createElement("div");
+const buttons = document.querySelector(".buttons");
+const results = document.querySelector(".results");
+const playAgain = document.createElement("button");
+
+rockButton.classList.toggle("rockButton");
+paperButton.classList.toggle("paperButton");
+scissorsButton.classList.toggle("scissorsButton");
+statusContainer.classList.toggle("info");
+scoreContainer.classList.toggle("scores");
+
+rockButton.textContent = "Rock";
+paperButton.textContent = "Paper";
+scissorsButton.textContent = "Scissors";
+playAgain.textContent = "PLAY AGAIN";
+
+buttons.appendChild(rockButton);
+buttons.appendChild(paperButton);
+buttons.appendChild(scissorsButton);
+results.appendChild(statusContainer);
+results.appendChild(scoreContainer);
+
+rockButton.addEventListener("click", () => {playRound("rock", getComputerChoice())});
+paperButton.addEventListener("click", () => {playRound("paper", getComputerChoice())});
+scissorsButton.addEventListener("click", () => {playRound("scissors", getComputerChoice())});
+playAgain.addEventListener("click", play);
